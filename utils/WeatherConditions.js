263e432c49f1765cd535d2d553b896e6532dff27
Eric Mauldin <emauldin84@gmail.com>
2019-06-14 14:39:50 -0400

    adds WeatherConditions to utils folder

---
+++ utils/WeatherConditions.js
@@ -0,0 +1,50 @@
+export const weatherConditions = {
+    Rain: {
+    color: '#005BEA',
+    title: 'Raining',
+    subtitle: 'Get a cup of coffee',
+    icon: 'shower'
+    },
+    Clear: {
+    color: '#f7b733',
+    title: 'So Sunny',
+    subtitle: 'It is hurting my eyes',
+    icon: 'sun'
+    },
+    Thunderstorm: {
+    color: '#616161',
+    title: 'A Storm is coming',
+    subtitle: 'Because Gods are angry',
+    icon: 'bolt'
+    },
+    Clouds: {
+    color: '#1F1C2C',
+    title: 'Clouds',
+    subtitle: 'Everywhere',
+    icon: 'cloud'
+    },
+    Snow: {
+    color: '#00d2ff',
+    title: 'Snow',
+    subtitle: 'Get out and build a snowman for me',
+    icon: 'snowflake'
+    },
+    Drizzle: {
+    color: '#076585',
+    title: 'Drizzle',
+    subtitle: 'Partially raining...',
+    icon: 'shower'
+    },
+    Haze: {
+    color: '#66A6FF',
+    title: 'Haze',
+    subtitle: 'Another name for Partial Raining',
+    icon: 'cloud'
+    },
+    Mist: {
+    color: '#3CD3AD',
+    title: 'Mist',
+    subtitle: "Don't roam in forests!",
+    icon: 'shower'
+    }
+};
